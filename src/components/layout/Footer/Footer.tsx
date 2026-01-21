2e040baf10a2901b26bb8d976a95421c078c21e6
The Hoang Nguyen <62173539+thehoang21@users.noreply.github.com>
2026-01-21 16:32:17 +0700

    Update Footer.tsx

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -4,16 +4,16 @@ export const Footer: React.FC = () => {
   return (
     <footer 
       className="flex border-t border-slate-200 bg-white py-2.5 md:py-3 lg:py-3.5 px-4 md:px-6 lg:px-8 flex-col md:flex-row justify-between items-center gap-2 md:gap-0 text-xs text-slate-500"
-      style={{
-        paddingBottom: 'calc(0.625rem + var(--safe-area-inset-bottom))', // 0.625rem = py-2.5
-      }}
+      // style={{
+      //   paddingBottom: 'calc(0.625rem + var(--safe-area-inset-bottom))', // 0.625rem = py-2.5
+      // }}
     >
       <div className="text-center md:text-left">
         &copy; {new Date().getFullYear()} Ngoc Thien Pharma.{" "}
-        <span className="hidden md:inline">All rights reserved. EU-GMP Compliant.</span>
+        <span className="hidden md:inline">All rights reserved.</span>
       </div>
       <div className="hidden md:flex items-center space-x-2 md:space-x-4">
-        <span>Version 1.0.1 (Dev)</span>
+        <span>Version 1.0.1</span>
       </div>
     </footer>
   );
